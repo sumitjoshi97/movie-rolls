a8280c8f24c826167dd9985ceec9bb3d86500eec
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-19 17:53:02 +0530

    chore: add header data

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
   render() {
-    return <div className='header'>header</div>
+    return (
+      <div className='header'>
+        <div className='header__brand'>Movie Rolls</div>
+        <div className='header__left'>
+          <div className='header__left__search'>search</div>
+          <div className='header__left__browse'>browse</div>
+        </div>
+        <div className='header__right'>
+          <div className='header__right__profile'>profile</div>
+        </div>
+      </div>
+    )
   }
 }
