4049f719174793ad16aed3ee7a77b4eb00472bb0
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-20 20:48:08 +0530

    style: change div to lists

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
+import './styles.scss'
 
 export default class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <div className='header__brand'>Movie Rolls</div>
-        <div className='header__left'>
-          <div className='header__left__search'>search</div>
-          <div className='header__left__browse'>browse</div>
-        </div>
-        <div className='header__right'>
-          <div className='header__right__profile'>profile</div>
-        </div>
+        <ul className='header__left'>
+          <li className='header__left__brand'>Movie Rolls</li>
+          <li className='header__left__search'>search</li>
+          <li className='header__left__browse'>browse</li>
+        </ul>
+        <ul className='header__right'>
+          <li className='header__right__profile'>profile</li>
+        </ul>
       </div>
     )
   }
