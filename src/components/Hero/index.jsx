0e1b779100b65b6a5f177155c5b5a792b3db3021
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-20 20:48:34 +0530

    chore: add hero content

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
+import './styles.scss'
 
 export default class Hero extends Component {
   render() {
-    return <div className='hero'>hero</div>
+    return (
+      <div className='hero'>
+        <div className='hero__info'>
+          <div className='hero__info__img' />
+          <div className='hero__info__text'>
+            <h1 className='hero__info__text__header'>something</h1>
+            <p className='hero__info__text__summary'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam in
+              cupiditate ad modi. Exercitationem, veritatis rat
+            </p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
