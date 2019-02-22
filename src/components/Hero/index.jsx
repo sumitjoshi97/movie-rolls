3dca2e90654975e541d93d959a2452ade5d68645
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-22 22:26:25 +0530

    chore: update hero component with current movies

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,21 +1,74 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
+
 import './styles.scss'
 
-export default class Hero extends Component {
+class Hero extends Component {
+  componentDidMount() {
+    this.props.fetchCurrentMovies()
+  }
   render() {
-    return (
-      <div className='hero'>
-        <div className='hero__info'>
-          <div className='hero__info__img' />
+    const { currentMovies } = this.props
+
+    let data = <div>to be loaded</div>
+    if (currentMovies) {
+      const heroBackdrop = {
+        background: `https://image.tmdb.org/t/p/original/${
+          currentMovies.results[0].backdrop_path
+        }, rgba(0,0,0,0.45)`
+      }
+
+      data = (
+        <div className='hero__info' style={heroBackdrop}>
+          <img
+            src={`https://image.tmdb.org/t/p/w185/${
+              currentMovies.results[0].poster_path
+            }`}
+            alt=''
+            className='hero__info__img'
+          />
           <div className='hero__info__text'>
-            <h1 className='hero__info__text__header'>something</h1>
+            <h1 className='hero__info__text__header'>
+              {currentMovies.results[0].title}
+            </h1>
+            <span className='hero__info__text__rating'>
+              {currentMovies.results[0].vote_average}
+            </span>
+            <span className='hero__info__text__adult'>
+              {currentMovies.results[0].adult}
+            </span>
+            <span className='hero__info__text__language'>
+              {currentMovies.results[0].original_language}
+            </span>
+            <span className='hero__info__text__year'>
+              {currentMovies.results[0].release_date}
+            </span>
             <p className='hero__info__text__summary'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam in
-              cupiditate ad modi. Exercitationem, veritatis rat
+              {currentMovies.results[0].overview}
             </p>
+            <button className='hero__info__favorite-btn'>
+              add to favorite
+            </button>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
+
+    return <div className='hero'>{data}</div>
   }
 }
+
+const mapStateToProps = state => ({
+  latest: state.movies.latest,
+  currentMovies: state.movies.currentMovies
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchCurrentMovies: () => dispatch(actions.getCurrentMovies())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Hero)
