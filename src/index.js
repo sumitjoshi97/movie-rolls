48b4a18241ec8b80a31766a6fd500e3cd2955e33
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-22 22:25:21 +0530

    chore: add redux store to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
+
+import { store } from './store'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
 
 serviceWorker.register()
