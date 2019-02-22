e0e7608595e27d505527706949603c6adc1087cb
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-22 22:24:32 +0530

    chore: add action to get Latest movies

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,19 +1,35 @@
 import * as actionTypes from './actionTypes'
+import { setError } from './index'
 import axios from '../axios-orders'
 
-export const setMovies = movies => {
-  return {
-    type: actionTypes.SET_CURRENT_MOVIES,
-    movies
-  }
+export const setCurrentMovies = movies => ({
+  type: actionTypes.SET_CURRENT_MOVIES,
+  movies
+})
+
+export const setLatest = (latest, images) => ({
+  type: actionTypes.SET_LATEST,
+  latest,
+  images
+})
+
+export const getLatestMovie = () => async dispatch => {
+  const latest = await axios.get(
+    `/movie/latest?api_key=${
+      process.env.REACT_APP_TMDB_API
+    }&language=en-US&page=1`
+  )
+
+  return await dispatch(setLatest(latest.data))
 }
 
 export const getCurrentMovies = () => async dispatch => {
-  axios
+  return axios
     .get(
       `/movie/now_playing?api_key=${
         process.env.REACT_APP_TMDB_API
       }&language=en-US&page=1`
     )
-    .then(res => dispatch(setMovies(res.data)))
+    .then(res => dispatch(setCurrentMovies(res.data)))
+    .catch(err => dispatch(setError()))
 }
