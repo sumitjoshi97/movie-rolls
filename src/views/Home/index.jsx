f4c19b12f46c02efa23b2ec952866bac21b7882f
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-23 19:02:51 +0530

    chore: move hero rendering logic to home  view component

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,17 +1,65 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
 import Nav from '../../components/Nav'
+import List from '../../components/List'
 
-export default class Home extends Component {
+import * as actions from '../../actions'
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.fetchCurrentMovies()
+    this.props.fetchCurrentShows()
+  }
+
+  renderHero = () => {
+    const { currentMovies, currentShows } = this.props
+
+    let renderData = []
+    renderData.push(
+      currentMovies.results[0],
+      currentMovies.results[1],
+      currentShows.results[0],
+      currentShows.results[1]
+    )
+
+    return renderData.map(slide => (
+      <Hero
+        title={slide.title}
+        poster={slide.poster_path}
+        backdrop={slide.backdrop_path}
+        rating={slide.vote_average}
+        summary={slide.overview}
+        adult={slide.adult}
+        year={slide.release_date.split('-')[0]}
+      />
+    ))
+  }
   render() {
     return (
       <div className='home'>
         <Header />
         <Hero />
         <Nav />
+        <List />
+        <List />
       </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  latest: state.movies.latest,
+  currentMovies: state.movies.currentMovies
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchCurrentMovies: () => dispatch(actions.getCurrentMovies())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
