71b71a5a3c7c9665790e1d574ba4d0e47b2eb330
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-24 23:29:56 +0530

    chore: render hero as per type - moviw/show

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Slider from 'react-slick'
+import PropTypes from 'prop-types'
 
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
@@ -8,7 +10,28 @@ import List from '../../components/List'
 
 import * as actions from '../../actions'
 
+import './styles.scss'
+
+const sliderOptions = {
+  arrows: false,
+  dots: true,
+  infinite: true,
+  vertical: true,
+  verticalSwiping: true,
+  fade: true,
+  speed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: 'linear'
+}
+
 class Home extends Component {
+  static propTypes = {
+    fetchCurrentMovies: PropTypes.func
+  }
+
   componentDidMount() {
     this.props.fetchCurrentMovies()
     this.props.fetchCurrentShows()
@@ -18,30 +41,54 @@ class Home extends Component {
     const { currentMovies, currentShows } = this.props
 
     let renderData = []
-    renderData.push(
-      currentMovies.results[0],
-      currentMovies.results[1],
-      currentShows.results[0],
-      currentShows.results[1]
-    )
 
-    return renderData.map(slide => (
-      <Hero
-        title={slide.title}
-        poster={slide.poster_path}
-        backdrop={slide.backdrop_path}
-        rating={slide.vote_average}
-        summary={slide.overview}
-        adult={slide.adult}
-        year={slide.release_date.split('-')[0]}
-      />
-    ))
+    if (currentMovies && currentShows) {
+      renderData.push(
+        { data: currentMovies.results[0], type: 'movie' },
+        { data: currentMovies.results[1], type: 'movie' },
+        { data: currentShows.results[0], type: 'show' },
+        { data: currentShows.results[1], type: 'show' }
+      )
+
+      return renderData.map(slide => {
+        if (slide.type === 'movie') {
+          return (
+            <Hero
+              key={slide.data.id}
+              title={slide.data.title}
+              poster={slide.data.poster_path}
+              backdrop={slide.data.backdrop_path}
+              rating={slide.data.vote_average}
+              type={}
+              summary={slide.data.overview}
+              adult={slide.data.adult}
+              year={slide.data.release_date.split('-')[0]}
+            />
+          )
+        } else {
+          return (
+            <Hero
+              key={slide.data.id}
+              title={slide.data.name}
+              poster={slide.data.poster_path}
+              backdrop={slide.data.backdrop_path}
+              rating={slide.data.vote_average}
+              type={slide.type}
+              summary={slide.data.overview}
+              year={slide.data.first_air_date.split('-')[0]}
+            />
+          )
+        }
+      })
+    }
+
+    return renderData
   }
   render() {
     return (
       <div className='home'>
         <Header />
-        <Hero />
+        <Slider {...sliderOptions}>{this.renderHero()}</Slider>
         <Nav />
         <List />
         <List />
@@ -52,11 +99,13 @@ class Home extends Component {
 
 const mapStateToProps = state => ({
   latest: state.movies.latest,
-  currentMovies: state.movies.currentMovies
+  currentMovies: state.movies.currentMovies,
+  currentShows: state.shows.currentShows
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchCurrentMovies: () => dispatch(actions.getCurrentMovies())
+  fetchCurrentMovies: () => dispatch(actions.getCurrentMovies()),
+  fetchCurrentShows: () => dispatch(actions.getCurrentShows())
 })
 
 export default connect(
