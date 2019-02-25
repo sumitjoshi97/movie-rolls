b3e24c9494e69d965f65d62aa2fbaea93412ab52
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-25 23:05:11 +0530

    chore: add actions for tv-shows

--- src/actions/shows.js
+++ src/actions/shows.js
@@ -17,3 +17,67 @@ export const getCurrentShows = () => async dispatch => {
     .then(res => dispatch(setCurrentShows(res.data)))
     .catch(err => dispatch(setError()))
 }
+
+export const setPopularShows = shows => ({
+  type: actionTypes.SET_POPULAR_SHOWS,
+  shows
+})
+
+export const getPopularShows = () => async dispatch => {
+  return axios
+    .get(
+      `/tv/popular?api_key=${
+        process.env.REACT_APP_TMDB_API
+      }&language=en-US&page=1`
+    )
+    .then(res => dispatch(setPopularShows(res.data)))
+    .catch(err => dispatch(setError()))
+}
+
+export const setAiringTodayShows = shows => ({
+  type: actionTypes.SET_AIRING_TODAY_SHOWS,
+  shows
+})
+
+export const getAiringTodayShows = () => async dispatch => {
+  return axios
+    .get(
+      `/tv/airing_today?api_key=${
+        process.env.REACT_APP_TMDB_API
+      }&language=en-US&page=1`
+    )
+    .then(res => dispatch(setAiringTodayShows(res.data)))
+    .catch(err => dispatch(setError()))
+}
+
+export const setTopShows = shows => ({
+  type: actionTypes.SET_TOP_SHOWS,
+  shows
+})
+
+export const getTopShows = () => async dispatch => {
+  return axios
+    .get(
+      `/tv/top_rated?api_key=${
+        process.env.REACT_APP_TMDB_API
+      }&language=en-US&page=1`
+    )
+    .then(res => dispatch(setTopShows(res.data)))
+    .catch(err => dispatch(setError()))
+}
+
+export const setSimilarShows = shows => ({
+  type: actionTypes.SET_SIMILAR_SHOWS,
+  shows
+})
+
+export const getSimilarShows = showId => async dispatch => {
+  return axios
+    .get(
+      `/tv/${showId}/similar?api_key=${
+        process.env.REACT_APP_TMDB_API
+      }&language=en-US&page=1`
+    )
+    .then(res => dispatch(setSimilarShows(res.data)))
+    .catch(err => dispatch(setError()))
+}
