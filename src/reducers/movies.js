060857c4eb5005a7d0587226b9ab36eac6cc34b4
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-25 23:08:48 +0530

    chore: add reducer

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,19 +1,31 @@
 import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-  latest: [],
-  currentMovies: null,
-  popularMovies: null,
-  popularShows: null
+  latest: null,
+  current: null,
+  popular: null,
+  top: null,
+  upcoming: null,
+  similar: null
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.SET_LATEST:
-      return { ...state, latest: [...state.latest, { ...action.latest }] }
-
     case actionTypes.SET_CURRENT_MOVIES:
-      return { ...state, currentMovies: action.movies }
+      return { ...state, current: action.movies }
+
+    case actionTypes.SET_UPCOMING_MOVIES:
+      return { ...state, upcoming: action.movies }
+
+    case actionTypes.SET_POPULAR_MOVIES:
+      return { ...state, popular: action.movies }
+
+    case actionTypes.SET_TOP_MOVIES:
+      return { ...state, top: action.movies }
+
+    case actionTypes.SET_SIMILAR_MOVIES:
+      return { ...state, top: action.movies }
+
     default:
       return state
   }
--- src/reducers/shows.js
+++ src/reducers/shows.js
@@ -1,19 +1,31 @@
 import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-  latest: [],
-  currentShows: null,
-  popularShows: null,
-  popularShows: null
+  latest: null,
+  current: null,
+  popular: null,
+  top: null,
+  airingToday: null,
+  similar: null
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.SET_LATEST:
-      return { ...state, latest: [...state.latest, { ...action.latest }] }
-
     case actionTypes.SET_CURRENT_SHOWS:
-      return { ...state, currentShows: action.shows }
+      return { ...state, current: action.shows }
+
+    case actionTypes.SET_AIRING_TODAY_SHOWS:
+      return { ...state, airingToday: action.shows }
+
+    case actionTypes.SET_POPULAR_SHOWS:
+      return { ...state, popular: action.shows }
+
+    case actionTypes.SET_TOP_SHOWS:
+      return { ...state, top: action.shows }
+
+    case actionTypes.SET_SIMILAR_SHOWS:
+      return { ...state, top: action.shows }
+
     default:
       return state
   }
