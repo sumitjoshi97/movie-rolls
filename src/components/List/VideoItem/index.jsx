1509497959f11c641ce011a35dc30f9cd09396c5
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-28 16:18:44 +0530

    chore: add video-item component

---
+++ src/components/List/VideoItem/index.jsx
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default class VideoItem extends PureComponent {
+  static propTypes = {
+    video: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    category: PropTypes.string
+  }
+
+  render() {
+    const { video, name, category } = this.props
+
+    return (
+      <div className='list-item'>
+        <iframe
+          className='list-item__video'
+          title='1'
+          src={`https://www.youtube.com/embed/${video}`}
+        />
+        <div className='list__item__name'>{name}</div>
+        <div className='list__item__name__sub'>{category}</div>
+      </div>
+    )
+  }
+}
