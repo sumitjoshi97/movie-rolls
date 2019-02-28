5355c4316a7782d95300b2551f2973659c09c9fc
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-28 16:17:58 +0530

    chore: add switch to render different list types

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import Slider from 'react-slick'
+
 import ListItem from './ListItem'
+import CastItem from './CastItem'
+import VideoItem from './VideoItem'
+
 import './styles.scss'
 
 const sliderOptions = {
-      arrows: true,
+  arrows: true,
   dots: false,
   speed: 400,
   slidesToShow: 8,
@@ -16,6 +20,29 @@ export default class List extends PureComponent {
   renderList = () => {
     const { type, items } = this.props
 
+    switch (type) {
+      case 'cast':
+        return items.map(item => (
+          <CastItem
+            key={item.id}
+            name={item.name}
+            poster={item.profile_path}
+            role={item.character}
+          />
+        ))
+
+      case 'video':
+        return items.map(item => (
+          <VideoItem
+            key={item.id}
+            name={item.name}
+            video={item.key}
+            category={item.type}
+          />
+        ))
+
+      //for movies / tv shows
+      default:
         return items.map(item => (
           <ListItem
             key={item.id}
@@ -27,6 +54,7 @@ export default class List extends PureComponent {
           />
         ))
     }
+  }
 
   render() {
     const { name } = this.props
