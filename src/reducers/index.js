e2adb7ab52377d48b623fd917c0edd38102506e1
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-28 16:19:05 +0530

    chore: add item reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import movies from './movies'
 import shows from './shows'
+import item from './item'
 
 export default combineReducers({
   movies,
-  shows
+  shows,
+  item
 })
---
+++ src/reducers/item.js
@@ -0,0 +1,26 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initialState = {
+  type: 'movie',
+  current: null,
+  credits: null,
+  videos: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_ITEM_TYPE:
+      return { ...state, type: action.itemType }
+
+    case actionTypes.SET_QUERY_ITEM:
+      return { ...state, current: action.item }
+
+    case actionTypes.SET_QUERY_ITEM_DETAILS:
+      return { ...state, credits: action.credits }
+
+    case actionTypes.SET_QUERY_ITEM_VIDEOS:
+      return { ...state, videos: action.videos }
+    default:
+      return state
+  }
+}
