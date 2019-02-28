849e34575c9920f87b9e2565a93d3107ee002965
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-02-28 16:16:39 +0530

    chore: render list items

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,25 +3,37 @@ import Slider from 'react-slick'
 import ListItem from './ListItem'
 import './styles.scss'
 
-export default class List extends Component {
-  render() {
-    const settings = {
+const sliderOptions = {
       arrows: true,
-      speed: 100,
-      slidesToShow: 4,
-      slidesToScroll: 1
+  dots: false,
+  speed: 400,
+  slidesToShow: 8,
+  swipeToSlide: true,
+  infinite: false
+}
+
+export default class List extends PureComponent {
+  renderList = () => {
+    const { type, items } = this.props
+
+        return items.map(item => (
+          <ListItem
+            key={item.id}
+            type={type}
+            pageId={item.id}
+            name={item.title}
+            rating={item.vote_average}
+            poster={item.poster_path}
+          />
+        ))
     }
+
+  render() {
+    const { name } = this.props
     return (
       <div className='list'>
-        <h2 className='list__header'>list</h2>
-        <Slider {...settings}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-        </Slider>
+        <h2 className='list__header'>{name}</h2>
+        <Slider {...sliderOptions}>{this.renderList()}</Slider>
       </div>
     )
   }
