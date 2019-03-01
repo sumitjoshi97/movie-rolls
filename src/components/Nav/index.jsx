2d913786f657845e3f91c3d02c4baf37d6004363
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-01 23:45:42 +0530

    chore: add update state to nav item

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,11 +4,31 @@ import './styles.scss'
 
 export default class Nav extends Component {
   state = {
-    isActive: [false, false]
+    isActive: [true, false]
+  }
+
+  setActive = index => {
+    let { isActive } = this.state
+    if (!isActive[index]) {
+      isActive[0] = !isActive[0]
+      isActive[1] = !isActive[1]
+    }
+    console.log(isActive)
+    this.setState({
+      isActive
+    })
   }
 
   renderNav = () => {
-    return this.props.options.map(option => <NavItem name={option} />)
+    return this.props.options.map((option, index) => (
+      <NavItem
+        name={option}
+        key={index}
+        index={index}
+        isActive={this.state.isActive[index]}
+        setActive={this.setActive}
+      />
+    ))
   }
 
   render() {
