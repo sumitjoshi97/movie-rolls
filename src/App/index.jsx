a6ee622053a6f281131f87a9c893738f10dfedc7
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-01 15:35:48 +0530

    chore: add react-router to app

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Header from '../components/Header'
 import Home from '../views/Home'
+import ItemDetail from '../views/ItemDetail'
+import Footer from '../components/Footer'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Home />
+        <Router>
+          <>
+            <Header />
+            <Route exact path='/details/:itemId' component={ItemDetail} />
+            <Route exact path='/' component={Home} />
+            <Footer />
+          </>
+        </Router>
       </div>
     )
   }
