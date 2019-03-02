576ad4ab565fd5f7139bb9f3e419376066f2e263
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-02 11:57:30 +0530

    chore: add setter actions for auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,19 +1,32 @@
 import * as actionTypes from './actionTypes'
 
+import { setError } from './index'
 import axios from '../axios-orders'
 
+////////////////////////////////////////////////////////////
+// set guest session for tmdb
 export const setGuestSession = session => ({
   type: actionTypes.SET_GUEST_SESSION,
   session
 })
 
-export const createGuestSession = () => {
-  return axios.get(
-    `/authentication/guest_session/new?api_key=${
-      process.env.REACT_APP_TMDB_API
-    }`
-  )
+export const startGuestSession = () => dispatch => {
+  return axios
+    .get(
+      `/authentication/guest_session/new?api_key=${
+        process.env.REACT_APP_TMDB_API
+      }`
+    )
+    .then(res => dispatch(setGuestSession(res.data)))
+    .catch(err => setError())
 }
+
+////////////////////////////////////////////////////////////
+// set request token for auth login
+export const setRequestToken = token => ({
+  type: actionTypes.SET_REQUEST_TOKEN,
+  token
+})
 
 export const getRequestToken = () => {
   return axios
@@ -21,7 +34,15 @@ export const getRequestToken = () => {
     .then(res => localStorage.setItem('token', res.data))
 }
 
-export const createSession = () => {
+/////////////////////////////////////////////////////////////
+// create auth session -
+
+export const setSession = session => ({
+  type: actionTypes.SET_SESSION,
+  session
+})
+
+export const startSession = () => {
   const token = localStorage.getItem('token')
 
   if (token.expires < new Date() * 1000) {
@@ -33,15 +54,24 @@ export const createSession = () => {
   )
 }
 
-export const logout = () => {
+/////////////////////////////////////////////////////////////
+//logout
+export const successlogout = () => ({
+  type: actionTypes.SUCCESS_LOGOUT
+})
+
+export const startlogout = () => dispatch => {
   const sessionId = localStorage.getItem('session')
 
   const data = {
     session_id: sessionId
   }
 
-  return axios.delete(
-    `/authentication/session?api_key=${process.env.REACT_APP_TMDB_API}`,
-    data
-  )
+  return axios
+    .delete(
+      `/authentication/session?api_key=${process.env.REACT_APP_TMDB_API}`,
+      data
+    )
+    .then(res => dispatch(logoutSuccess()))
+    .catch(err => setError())
 }
