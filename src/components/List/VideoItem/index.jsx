3fe39cf5cfbe4d288af50078180d138fc86abbd7
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-05 21:11:28 +0530

    chore: fix class names

--- src/components/List/VideoItem/index.jsx
+++ src/components/List/VideoItem/index.jsx
@@ -16,10 +16,11 @@ export default class VideoItem extends PureComponent {
         <iframe
           className='list-item__video'
           title='1'
+          width='180'
           src={`https://www.youtube.com/embed/${video}`}
         />
-        <div className='list__item__name'>{name}</div>
-        <div className='list__item__name__sub'>{category}</div>
+        <div className='list-item__name'>{name}</div>
+        <div className='list-item__name__sub'>{category}</div>
       </div>
     )
   }
