d6afc9cb4c0b156521976e00115acf243db99425
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-05 21:09:14 +0530

    chore: add classname

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 
+import './styles.scss'
+
 export default class Footer extends Component {
   render() {
-    return <div>footer</div>
+    return <div className='footer'>footer</div>
   }
 }
