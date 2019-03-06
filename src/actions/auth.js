8d0d3f850f49ae8233606934e3c4d1f7bc6342f8
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-06 14:34:29 +0530

    chore: add auth actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,77 +1,47 @@
 import * as actionTypes from './actionTypes'
 
 import { setError } from './index'
-import axios from '../axios-orders'
+import { authRef, facebookProvider, googleProvider } from '../config/firebase'
 
-////////////////////////////////////////////////////////////
-// set guest session for tmdb
-export const setGuestSession = session => ({
-  type: actionTypes.SET_GUEST_SESSION,
-  session
+///////////////////////////////////////////////////////////
+export const fetchUserSuccess = user => ({
+  type: actionTypes.FETCH_USER_SUCCESS,
+  user
 })
 
-export const startGuestSession = () => dispatch => {
-  return axios
-    .get(
-      `/authentication/guest_session/new?api_key=${
-        process.env.REACT_APP_TMDB_API
-      }`
-    )
-    .then(res => dispatch(setGuestSession(res.data)))
+//fetch user if logged in
+export const fetchUser = () => dispatch => {
+  authRef.onAuthStateChanged(user => {
+    if (user) {
+      dispatch(fetchUserSuccess(user))
+    } else {
+      dispatch(fetchUserSuccess(null))
+}
+})
+}
+
+//sign up user
+export const createUser = (email, password) => dispatch => {
+  authRef
+    .createUserWithEmailAndPassword(email, password)
+    .then(res => console.log('success'))
     .catch(err => setError())
 }
 
-////////////////////////////////////////////////////////////
-// set request token for auth login
-export const setRequestToken = token => ({
-  type: actionTypes.SET_REQUEST_TOKEN,
-  token
-})
-
-export const getRequestToken = () => {
-  return axios
-    .get(`/authentication/token/new?api_key=${process.env.REACT_APP_TMDB_API}`)
-    .then(res => localStorage.setItem('token', res.data))
-}
-
 /////////////////////////////////////////////////////////////
-// create auth session -
-
-export const setSession = session => ({
-  type: actionTypes.SET_SESSION,
-  session
-})
-
-export const startSession = () => {
-  const token = localStorage.getItem('token')
-
-  if (token.expires < new Date() * 1000) {
-    getRequestToken()
-  }
-  return axios.post(
-    `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000`,
-    token
-  )
-}
-
-/////////////////////////////////////////////////////////////
-//logout
-export const successlogout = () => ({
-  type: actionTypes.SUCCESS_LOGOUT
-})
-
-export const startlogout = () => dispatch => {
-  const sessionId = localStorage.getItem('session')
-
-  const data = {
-    session_id: sessionId
+// login user with email password
+export const loginUserWithEmailPassword = (email, password) => dispatch => {
+  return authRef
+    .signInWithEmailAndPassword(email, password)
+    .then(res => console.log('success'))
+    .catch(err => setError())
   }
 
-  return axios
-    .delete(
-      `/authentication/session?api_key=${process.env.REACT_APP_TMDB_API}`,
-      data
-    )
-    .then(res => dispatch(logoutSuccess()))
+// login user with social
+export const loginUserWithSocial = provider => dispatch => {
+  const authProvider = provider === 'google' ? googleProvider : facebookProvider
+  return authRef
+    .signInWithPopup(authProvider)
+    .then(res => console.log('success'))
     .catch(err => setError())
 }
