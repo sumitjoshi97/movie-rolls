45548215b0329d95939b9f357998a92742c3d3a3
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-07 22:30:57 +0530

    refactor: remove unused imports

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -4,12 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Layout from '../components/Layout'
 import HeaderLayout from '../components/HeaderLayout'
 
-import Header from '../components/Header'
-import Home from '../views/Home'
-import ItemDetail from '../views/ItemDetail'
-import Auth from '../views/Auth'
-import Footer from '../components/Footer'
-
 export default class App extends Component {
   render() {
     return (
