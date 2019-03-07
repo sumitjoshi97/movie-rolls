22785df000832511f361ea141f703a2dddcaf42a
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-07 22:50:13 +0530

    chore: move auth form to component

--- src/views/Auth/index.jsx
+++ src/views/Auth/index.jsx
@@ -1,32 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
+import AuthForm from '../../components/AuthForm'
 
 import './styles.scss'
 
 class Auth extends Component {
   render() {
+    const isRedirect = this.props.isAuthenticated ? <Redirect to='/' /> : null
+
     return (
       <div className='auth'>
-        <div className='auth__box'>
-          Movie rolls
-          <div className='auth__box__action-btn'>
-            <button className='auth__box__action-btn__login'>Login</button>
-            <button className='auth__box__action-btn__guest'>
-              Browse as Guest
-            </button>
-          </div>
-        </div>
-        <div className='auth__terms'>
-          * browsing as guest has limited functionality
-        </div>
+        {isRedirect}
+        <AuthForm />
       </div>
     )
   }
 }
 
-const mapDispatchToProps = dispatch => ({})
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.user !== null
+})
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(Auth)
+export default connect(mapStateToProps)(Auth)
