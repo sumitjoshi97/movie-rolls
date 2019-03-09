157ff6138862f3b78725f9a9b4742ea8fc106b97
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-09 19:12:27 +0530

    chore: add fetchUser in DidMount hook

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Layout from '../components/Layout'
 import HeaderLayout from '../components/HeaderLayout'
 
-export default class App extends Component {
+import * as actions from '../actions'
+
+class App extends Component {
+  componentDidMount = () => {
+    this.props.fetchUser()
+  }
+
+  fetchLists = () => {
+    if (this.props.user) {
+      this.props.fetchList('watch', this.props.user.uid)
+      this.props.fetchList('favorite', this.props.user.uid)
+    }
+  }
+
   render() {
     return (
       <div className='app'>
+        {this.fetchLists()}
         <Router>
           <Switch>
             <Route path='/profile' component={Layout} />
@@ -18,3 +33,17 @@ export default class App extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  user: state.auth.user
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchUser: () => dispatch(actions.fetchUser()),
+  fetchList: (type, userId) => dispatch(actions.fetchList(type, userId))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
