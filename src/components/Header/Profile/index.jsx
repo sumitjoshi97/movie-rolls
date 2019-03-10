64fd17f284b9a42a510e78c5241a73567cc7369b
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-10 16:56:30 +0530

    chore: add profile picture on button

--- src/components/Header/Profile/index.jsx
+++ src/components/Header/Profile/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 import './styles.scss'
 
-const Header = ({ isAuth }) => {
+const Header = ({ isAuth, profileURL }) => {
   const [hidden, setHidden] = useState(true)
 
   return (
@@ -11,18 +11,24 @@ const Header = ({ isAuth }) => {
       {isAuth ? (
         <>
           <button
-            className='profile-btns'
+            className='profile-btn'
             onClick={() => {
               hidden ? setHidden(false) : setHidden(true)
             }}
           >
-            B
+            <img
+              src={
+                profileURL ? profileURL : require('../../../assets/profile.png')
+              }
+              className='profile-btn__img'
+              alt=''
+            />
           </button>
           {hidden ? null : (
-            <div className='profile-btns__list'>
-              <button className='profile-btns__list__favorite'>Favorite</button>
-              <button className='profile-btns__list__watch'>watch later</button>
-              <button className='profile-btns__list__logout'>logout</button>
+            <div className='profile-btn__list'>
+              <button className='profile-btn__list__favorite'>Favorite</button>
+              <button className='profile-btn__list__watch'>watch later</button>
+              <button className='profile-btn__list__logout'>logout</button>
             </div>
           )}
         </>
