938e2d170b84e700903747f14cf25491ccf664be
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-12 17:30:24 +0530

    chore: fix route

--- src/components/HeaderLayout/index.jsx
+++ src/components/HeaderLayout/index.jsx
@@ -7,7 +7,7 @@ import Home from '../../views/Home'
 import ItemDetail from '../../views/ItemDetail'
 import Discover from '../../views/Discover'
 import Favorites from '../../views/Favorites'
-import WatchLaters from '../../views/WatchLaters';
+import WatchLaters from '../../views/WatchLaters'
 
 export default class HeaderLayout extends Component {
   state = {
@@ -38,7 +38,7 @@ export default class HeaderLayout extends Component {
           <Route exact path='/details/:itemId' component={ItemDetail} />
           <Route exact path='/discover' component={Discover} />
           <Route path='/profile/favorites' component={Favorites} />
-          <Route path='/profile/watch-later' component={WatchLaters}
+          <Route path='/profile/watch-later' component={WatchLaters} />
           <Route exact path='/' component={Home} />
           <Route render={() => <div>loading...</div>} />
         </Switch>
