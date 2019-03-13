1e77da186766302c672df95c362a08a6bd8ca714
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-13 13:24:32 +0530

    chore: add setItemType function to onClick event

--- src/components/UserList/UserListItem/index.jsx
+++ src/components/UserList/UserListItem/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const UserListItem = ({ pageId, name, poster }) => {
+import { setItemType } from '../../../actions'
+
+import './styles.scss'
+
+const UserListItem = ({ pageId, name, poster, type, ...props }) => {
   return (
     <div className='user-list-item'>
-      <Link to={`/details/${pageId}`}>
-        <img src={`https://image.tmdb.org/t/p/w185/${poster}`} alt={name} />
-        <p>{name}</p>
+      <Link to={`/details/${pageId}`} onClick={() => props.setItemType(type)}>
+        <img
+          src={`https://image.tmdb.org/t/p/w185/${poster}`}
+          alt={name}
+          className='user-list-item__img'
+        />
+        <p className='user-list-item__name'>{name}</p>
       </Link>
     </div>
   )
 }
 
-export default UserListItem
+export default connect(
+  null,
+  { setItemType }
+)(UserListItem)
