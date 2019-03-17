9e6e6e161e1c1e2fc542d80e337709ea1f535067
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-17 12:10:12 +0530

    chore: add logout reducer case

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -8,11 +8,13 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_USER_SUCCESS:
-      return { ...state, userId: action.user.uid }
+      return { ...state, userId: action.userId }
 
     case actionTypes.SET_REDIRECT_PATH:
       return { ...state, redirectPath: action.path }
 
+    case actionTypes.LOGOUT_SUCCESS:
+      return { ...state, userId: '', redirectPath: '/' }
     default:
       return state
   }
