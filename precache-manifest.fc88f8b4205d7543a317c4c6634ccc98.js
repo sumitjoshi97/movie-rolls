6b7b247d2fcb3ef5a9cf8e417a51979ff2615150
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-01-14 18:31:19 +0530

    Updates

---
+++ precache-manifest.fc88f8b4205d7543a317c4c6634ccc98.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de22b21b3aa8cfc8de0d70d86377e95b",
+    "url": "/movie-rolls/index.html"
+  },
+  {
+    "revision": "39649627cdb77c296763",
+    "url": "/movie-rolls/static/css/1.38a8a75d.chunk.css"
+  },
+  {
+    "revision": "011c493eff7d8f2c9bb5",
+    "url": "/movie-rolls/static/css/10.366bea29.chunk.css"
+  },
+  {
+    "revision": "c64cfeb89a28c0b6a180",
+    "url": "/movie-rolls/static/css/11.77d02257.chunk.css"
+  },
+  {
+    "revision": "7a3f017dea19b58673fa",
+    "url": "/movie-rolls/static/css/12.3cac09e8.chunk.css"
+  },
+  {
+    "revision": "7a9d2a905ad77e942729",
+    "url": "/movie-rolls/static/css/5.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "8c5d97105ff3811915ab",
+    "url": "/movie-rolls/static/css/6.3887efe3.chunk.css"
+  },
+  {
+    "revision": "95a7bd462a4a25013084",
+    "url": "/movie-rolls/static/css/7.8620cf8b.chunk.css"
+  },
+  {
+    "revision": "df8aab6e8d0f7fbbe205",
+    "url": "/movie-rolls/static/css/8.fe76c8a4.chunk.css"
+  },
+  {
+    "revision": "d8a5897e66306c264963",
+    "url": "/movie-rolls/static/css/9.185b6a69.chunk.css"
+  },
+  {
+    "revision": "cd6127cf2e3bb02e71a1",
+    "url": "/movie-rolls/static/css/main.f8138152.chunk.css"
+  },
+  {
+    "revision": "064669e6db2244f4ec0f",
+    "url": "/movie-rolls/static/js/0.ee8d0b25.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/movie-rolls/static/js/0.ee8d0b25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39649627cdb77c296763",
+    "url": "/movie-rolls/static/js/1.0e727900.chunk.js"
+  },
+  {
+    "revision": "011c493eff7d8f2c9bb5",
+    "url": "/movie-rolls/static/js/10.262b4b0e.chunk.js"
+  },
+  {
+    "revision": "c64cfeb89a28c0b6a180",
+    "url": "/movie-rolls/static/js/11.4ddd1532.chunk.js"
+  },
+  {
+    "revision": "7a3f017dea19b58673fa",
+    "url": "/movie-rolls/static/js/12.547985c8.chunk.js"
+  },
+  {
+    "revision": "46b5124bb70da7dc0f0c",
+    "url": "/movie-rolls/static/js/4.a10a999e.chunk.js"
+  },
+  {
+    "revision": "a37b512ddb5189c0a28b564fc708368d",
+    "url": "/movie-rolls/static/js/4.a10a999e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a9d2a905ad77e942729",
+    "url": "/movie-rolls/static/js/5.c515e755.chunk.js"
+  },
+  {
+    "revision": "8c5d97105ff3811915ab",
+    "url": "/movie-rolls/static/js/6.d2228fda.chunk.js"
+  },
+  {
+    "revision": "95a7bd462a4a25013084",
+    "url": "/movie-rolls/static/js/7.cee2533b.chunk.js"
+  },
+  {
+    "revision": "df8aab6e8d0f7fbbe205",
+    "url": "/movie-rolls/static/js/8.72ef9d1b.chunk.js"
+  },
+  {
+    "revision": "d8a5897e66306c264963",
+    "url": "/movie-rolls/static/js/9.d16ab792.chunk.js"
+  },
+  {
+    "revision": "cd6127cf2e3bb02e71a1",
+    "url": "/movie-rolls/static/js/main.431e80ba.chunk.js"
+  },
+  {
+    "revision": "80c317b5c84ebb1e75fd",
+    "url": "/movie-rolls/static/js/runtime-main.33dd86bb.js"
+  },
+  {
+    "revision": "9ca896c24be01e831908b89fd43734ed",
+    "url": "/movie-rolls/static/media/login_backdrop.9ca896c2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-rolls/precache-manifest.56f42bfca5a533cc38d71ccb41748d25.js"
+  "/movie-rolls/precache-manifest.fc88f8b4205d7543a317c4c6634ccc98.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movie-rolls/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-rolls/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
