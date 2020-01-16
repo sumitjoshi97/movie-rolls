6dfa49c0f701740d6aff512ca7ba8ee83d339f0b
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-01-16 16:55:31 +0530

    updated app routes

--- src/App/routes.js
+++ src/App/routes.js
@@ -2,11 +2,31 @@ import { lazy } from 'react'
 
 export default [
   {
-    component: lazy(() => import('../components/Layout')),
-    path: '/auth'
+    component: lazy(() => import('../views/Auth')),
+    path: '/auth',
   },
   {
-    component: lazy(() => import('../components/HeaderLayout')),
-    path: '/'
-  }
+    component: lazy(() => import('../views/SearchResults')),
+    path: '/search-results/:searchQuery',
+  },
+  {
+    component: lazy(() => import('../views/ItemDetail')),
+    path: '/details/:itemId',
+  },
+  {
+    component: lazy(() => import('../views/Discover')),
+    path: '/discover',
+  },
+  {
+    component: lazy(() => import('../views/Favorites')),
+    path: '/profile/favorites',
+  },
+  {
+    component: lazy(() => import('../views/WatchLaters')),
+    path: '/profile/watch-later',
+  },
+  {
+    component: lazy(() => import('../views/Home')),
+    path: '/',
+  },
 ]
